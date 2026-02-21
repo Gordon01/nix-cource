af1f00592bb40bc2e97c9cc8ab512a2ecf86dcb0
Alexander <etc9053@gmail.com>
2026-02-21 18:11:40 +0300

    intro: PDF export

--- flake.nix
+++ flake.nix
@@ -5,32 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs
-          pkgs.marp-cli
-        ];
-      };
-
       packages.${system}.intro = pkgs.stdenv.mkDerivation {
         name = "intro";
         src = ./slides;
 
-        buildInputs = [ pkgs.marp-cli ];
+        nativeBuildInputs = with pkgs; [
+          chromium
+          fontconfig
+          marp-cli
+          noto-fonts
+        ];
 
         buildPhase = ''
+          export HOME=$TMPDIR
+          export CHROME_PATH=${pkgs.chromium}/bin/chromium
+
           marp intro.md --html -o index.html
+          marp intro.md --pdf -o intro.pdf
         '';
 
         installPhase = ''
           mkdir -p $out
           cp index.html $out/
+          cp intro.pdf $out/
         '';
       };
     };
